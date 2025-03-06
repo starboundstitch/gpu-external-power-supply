969c5cbbcb489238906f969edb0ff4a144fbf52f
starboundstitch <personal@stitchy.moe>
2025-03-06 06:44:44 +0000

    feat: mode struct and impl
    This is for the different modes that the UI can be in. Currently, there
    is a navigation mode for when the user can navigate between the values
    and an update mode so that the user can change the value that is being
    hovered over.

--- firmware/src/navigation.rs
+++ firmware/src/navigation.rs
@@ -1,4 +1,13 @@
+pub enum Mode {
+    Navigation,
+    Update,
+}
 
+impl Default for Mode {
+    fn default() -> Mode {
+        Mode::Navigation
+    }
+}
 pub fn translate_point(point: (i32, i32)) -> Point {
     Point::new(27 + 6 * 9 * point.0, 16 + 16 * point.1)
 }
