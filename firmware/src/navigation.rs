5954a24d97c7e6d6eb6cfe7109ca484f26d46bcc
starboundstitch <personal@stitchy.moe>
2025-03-06 06:49:13 +0000

    feat: navigation struct
    This stores the current position of the user in the UI as well as
    updates the position. This means that all the position math and error
    checking is handled here and thus is easy to call from other parts in
    the code.

--- firmware/src/navigation.rs
+++ firmware/src/navigation.rs
@@ -1,3 +1,61 @@
+use embedded_graphics::prelude::Point;
+
+#[derive(Default)]
+pub struct Navigation {
+    // x, y
+    position: (i32, i32),
+    mode: Mode,
+}
+
+// Implements navigation across the microcontroller for the user input
+impl Navigation {
+    const X_MAX: i32 = 2;
+    const Y_MAX: i32 = 3;
+
+    pub fn get_position(&self) -> (i32, i32) {
+        self.position
+    }
+
+    pub fn get_point(&self) -> Point {
+        translate_point(self.position)
+    }
+
+    pub fn move_left(&mut self) {
+        if self.position.0 > 0 {
+            self.position.0 -= 1;
+        }
+    }
+
+    pub fn move_right(&mut self) {
+        if self.position.0 < Self::X_MAX - 1 {
+            self.position.0 += 1;
+        }
+    }
+
+    pub fn move_up(&mut self) {
+        if self.position.1 > 0 {
+            self.position.1 -= 1;
+        }
+    }
+
+    pub fn move_down(&mut self) {
+        if self.position.1 < Self::Y_MAX - 1 {
+            self.position.1 += 1;
+        }
+    }
+
+    pub fn change_mode(&mut self) {
+        match self.mode {
+            Mode::Navigation => self.mode = Mode::Update,
+            Mode::Update => self.mode = Mode::Navigation,
+        }
+    }
+
+    pub fn get_mode(&self) -> &Mode {
+        &self.mode
+    }
+}
+
 pub enum Mode {
     Navigation,
     Update,
@@ -8,6 +66,7 @@ impl Default for Mode {
         Mode::Navigation
     }
 }
+
 pub fn translate_point(point: (i32, i32)) -> Point {
     Point::new(27 + 6 * 9 * point.0, 16 + 16 * point.1)
 }
